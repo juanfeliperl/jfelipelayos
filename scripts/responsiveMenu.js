e001d468f7d7e7f391d499b99a2eb2417baabe04
Felipe Layos <jfelipelayos@gmail.com>
2021-10-10 18:53:08 -0500

    Fixed menu close when section is clicked

--- scripts/responsiveMenu.js
+++ scripts/responsiveMenu.js
@@ -3,8 +3,18 @@ window.addEventListener('load', () => {
   const closeMenuBtn = document.querySelector('.header-nav-open');
   const openMenuBtn = document.querySelector('.nav-list-item-close');
 
+  const aboutMeBtn = document.querySelector('.about-me');
+  const techStackBtn = document.querySelector('.tech-stack');
+  const projectsBtn = document.querySelector('.projects');
+
+
+
   openMenuBtn.addEventListener('click', closeMenu)
   closeMenuBtn.addEventListener('click', openMenu)
+  aboutMeBtn.addEventListener('click', closeMenu);
+  techStackBtn.addEventListener('click', closeMenu);
+  projectsBtn.addEventListener('click', closeMenu);
+
 
   function openMenu() {
     navMenu.style.display = 'flex';
